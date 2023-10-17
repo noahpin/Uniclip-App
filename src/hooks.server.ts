4eb79e20beded7e18c3df13a2ae76edd93aee43b
dazon <44048306+noahpin@users.noreply.github.com>
2023-10-16 23:32:12 -0700

    yeah so theres a lot

---
+++ src/hooks.server.ts
@@ -0,0 +1,27 @@
+// src/hooks.server.ts
+import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
+import type { Handle } from '@sveltejs/kit'
+
+export const handle: Handle = async ({ event, resolve }) => {
+  event.locals.supabase = createSupabaseServerClient({
+    supabaseUrl: import.meta.env.VITE_SUPABASE_URL,
+    supabaseKey: import.meta.env.VITE_SUPABASE_ANON_KEY,
+    event,
+  })
+
+  /**
+   * A convenience helper so we can just call await getSession() instead const { data: { session } } = await supabase.auth.getSession()
+   */
+  event.locals.getSession = async () => {
+    const {
+      data: { session },
+    } = await event.locals.supabase.auth.getSession()
+    return session
+  }
+
+  return resolve(event, {
+    filterSerializedResponseHeaders(name) {
+      return name === 'content-range'
+    },
+  })
+}
